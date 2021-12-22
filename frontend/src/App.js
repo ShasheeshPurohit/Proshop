f43db1249f9261c528ed35ecd7668c4a928e4cc8
ShasheeshPurohit <75235962+ShasheeshPurohit@users.noreply.github.com>
2021-12-22 13:37:30 +0530

    Router set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,24 @@
+import {Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
+
 const App = () => {
   return (
     <>
-      <h1>Welcome to Proshop</h1>
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen/>} exact/>
+            <Route path={`/product/:id`} element={<ProductScreen/>}/>
+          </Routes>
+        </Container>
+      </main>
+      <Footer/>
     </>
   );
 }
---
+++ frontend/src/components/Rating/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons'
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons'
+import "./styles.css"
+
+const Rating = ({value, text, color}) => {
+    return (
+        <div className="rating">
+            <span>
+                <FontAwesomeIcon style={{color}} icon={value>=1?faStar:value>=0.5?faStarHalfAlt:farStar}/>
+            </span>
+            <span>
+                <FontAwesomeIcon style={{color}} icon={value>=2?faStar:value>=1.5?faStarHalfAlt:farStar}/>
+            </span>
+            <span>
+                <FontAwesomeIcon style={{color}} icon={value>=3?faStar:value>=2.5?faStarHalfAlt:farStar}/>
+            </span>
+            <span>
+                <FontAwesomeIcon style={{color}} icon={value>=4?faStar:value>=3.5?faStarHalfAlt:farStar}/>
+            </span>
+            <span>
+                <FontAwesomeIcon style={{color}} icon={value>=5?faStar:value>=4.5?faStarHalfAlt:farStar}/>
+            </span>
+            <span>
+                {text && text}
+            </span>
+        </div>
+    )
+}
+
+export default Rating
